c494bf701c84c8cdc7448ef5c221a01b2bcb7557
Maria.C <maria.cabulea22@gmail.com>
2024-08-01 19:45:24 +0200

    Shopping Manager App

--- src/obiecte_clase/Dog.java
+++ src/obiecte_clase/Dog.java
@@ -13,6 +13,16 @@ public class Dog {
         // atunci cand generam un contructor, constructorul default DISPARE
         // daca avem un constructor definit, putem defini in mod explicit constructorul default
     }
+    public Dog (String rasa) {
+
+    }
+    public Dog (int name) {
+
+    }
+
+    public Dog (String rasa, String name, int age) {
+
+    }
 
     public Dog (String rasa, int age, String name) {
         // constructor cu parametrii
@@ -29,13 +39,28 @@ public class Dog {
         return "Rasa: " + this.rasa + ", Varsta: " + this.age + ", Nume: " + this.name;
     }
 
-    public boolean equals(Object o) {
-        Dog obj = (Dog) o; // am facut cast de la object la Dog
+    public boolean equals(Object obiect) {
+
+        // metoda equals ne spune cand 2 obiecte sunt egale, in ce conditii. Noi stabilim cand 2 obiecte sunt egale
+
+        Dog obj = (Dog) obiect; // am facut cast de la object la Dog
 
         if (this.name.equals(obj.name) && this.rasa.equals(obj.rasa) && this.age == obj.age) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public void apport() {
+        System.out.println("Apport");
+    }
+
+    public String displayInfo() {
+        return "Rasa: " + this.rasa + ", Varsta: " + this.age + ", Nume: " + this.name;
+    }
+
+    public static void bark () {
+        System.out.println("Ham ham!");
     }
 }
--- src/obiecte_clase/Main.java
+++ src/obiecte_clase/Main.java
@@ -1,5 +1,35 @@
 package obiecte_clase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *Keyword STATIC
+ * - se foloseste pentru: proprietati, metode, clase (nu se foloseste pentru constructori)
+ * - atunci cand o proprietate/variabila/clasa/metoda este statica, se aloca spatiu in memorie la pornirea aplicatiei
+ * - intr-o metoda statica NU putem apela proprietati (variabile) sau metode care nu sunt statice
+ * - pentru a apela o proprietate/metoda/clasa statica, folosim NUMELE CLASEI . NUMELE PRORIETATII/METODEI/CLASEI => Dog.bark() nu folosim instanta clasei, adica rex.bark()
+ * - elementele statice sunt independente de obiecte. Acestea depind de clasa, nu de instanta lor
+ *
+ * Definire variabile
+ *         // La definirea (declararea) unei variabile avem 2 elemente:
+ *         // 1. tipul variabilei (String, Integer, int, double, boolean, etc)
+ *         // 2. numele variabilei (rex, listaNumerePare, pret, isPresent, etc)
+ *
+ *         Integer number = 0;
+ *         //tip   nume_var = valoare;
+ *
+ *         // atunci cand folosim numele variabilei inseamna ca o apelam (folosim)
+ *         number = 10;
+ *         System.out.println(number);
+ *
+ *         //ATENTIE!!! La folosirea/apelul variabilei nu mai scriem tipul acesteia (folosim doar number, fara Integer)
+ *         // Integer number = 100;
+ *
+ *         String text = " ";
+ *         StringBuilder builder = null;
+ */
 public class Main {
     public static void main(String[] args) {
         Dog rex = new Dog("Labrador", 2, "Rex");
@@ -13,7 +43,7 @@ public class Main {
 
         // Dog rex2 = new Dog ("Ciobanesc", 2, "Rex"); -> resultat: Obiectele sunt egale
 
-        Dog rex2 = new Dog ("Ciobanesc", 2, "Rex");
+        Dog rex2 = new Dog ("Labrador", 2, "Rex");
 
         // NU FOLOSIM "==" ATUNCI CAND DORIM SA COMPARAM 2 OBIECTE
         if (rex == rex2) {
@@ -27,6 +57,80 @@ public class Main {
         } else {
             System.out.println("Obiectele nu sunt egale");
         }
+
+        Dog.bark(); // folodim clasa Dog si  nu obiectele de tipul clasei Dog
+        rex.apport(); // folosim obiectul si nu clasa Dog
+
+       // addDog();
+        dogManagers();
     }
 
+    public static Dog addDog () {
+        System.out.println("Add dog");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Insert race: ");
+        String race = scanner.nextLine();
+
+        System.out.println("Insert age: ");
+        int age = scanner.nextInt();
+
+        scanner.close();
+
+        // Dog - tipul variabilei, in acest caz clasa Dog
+        // variabilaDeTipDog - este numele variabilei
+        // = - operator de atribuire (initializare)
+        // new Dog() - reprezinta apelul contstructorului din clasa Dog
+
+        Dog variabilaDeTipDog = new Dog (race, age, name);
+
+        // La definirea (declararea) unei variabile avem 2 elemente:
+        // 1. tipul variabilei (String, Integer, int, double, boolean, etc)
+        // 2. numele variabilei (rex, listaNumerePare, pret, isPresent, etc)
+
+        Integer number = 0;
+        //tip   nume_var = valoare;
+
+        // atunci cand folosim numele variabilei inseamna ca o apelam (folosim)
+        number = 10;
+        System.out.println(number);
+
+        //ATENTIE!!! La folosirea/apelul variabilei nu mai scriem tipul acesteia (folosim doar number, fara Integer)
+        // Integer number = 100;
+
+        String text = " ";
+        StringBuilder builder = null;
+        return variabilaDeTipDog;
+
+    }
+
+    public static void dogManagers() {
+        List<Dog> dogList = new ArrayList<>();
+
+        Dog dog1 = new Dog("Bichon", 2, "Max");
+        Dog dog2 = new Dog("Labrador", 5, "Ben");
+        Dog dog3 = new Dog("Chihuahua", 3, "Picky");
+        Dog dog4 = addDog();
+        
+        dogList.add(dog1);
+        dogList.add(dog2);
+        dogList.add(dog3);
+        dogList.add(dog4);
+
+        for (int i = 0; i < dogList.size(); i++) {
+            Dog element = dogList.get(i);
+
+            System.out.println(element);
+
+        }
+        if (dogList.size()>4) {
+            return;
+            // atunci cand folosim "return;" intr-o metoda cu tipul returnat void, se opreste executia metodei
+        }
+        System.out.println("Dogs manager");
+    }
 }
